3c5c2da7a8effba0b15759b334a88594328fdd52
shorya-alt <shorayaraj1993@gmail.com>
2019-11-20 15:50:18 +0530

    code update 20th november 2019...............................

--- bankuserdetails/userbankdetails/models.py
+++ bankuserdetails/userbankdetails/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Bank(models.Model):
-    ifsc = models.CharField(max_length=11)  # branch
-    bank_id = models.IntegerField(max_length=50)
-    branch = models.CharField(max_length=80)
-    address = models.CharField(max_length=200)
-    city = models.CharField(max_length=50)
-    district = models.CharField(max_length=50)
-    state = models.CharField(max_length=30)
+    ifsc = models.CharField(max_length=11,null=True)  
+    bank_id = models.IntegerField(null=True)
+    branch = models.CharField(max_length=80,null=True)
+    address = models.CharField(max_length=200,null=True)
+    city = models.CharField(max_length=50,null=True)
+    district = models.CharField(max_length=50,null=True)
+    state = models.CharField(max_length=30,null=True)
     bank_name = models.CharField(max_length=50,null=True)
 
-    def __str__(self):
-        return f"{self.ifsc},{self.bank_id},{self.branch},{self.address},{self.city},{self.district},{self.state}"
+    #def __str__(self):
+        #return f"{self.ifsc},{self.bank_id},{self.branch},{self.address},{self.city},{self.district},{self.state}"
 
